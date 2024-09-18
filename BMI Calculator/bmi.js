849a7a5ac2f8d427192191c06548decab924d6e9
Safkat Sharif Sadli <safkatsharif@gmail.com>
2024-09-18 16:01:17 +0600

    BMI calculator working

--- BMI Calculator/bmi.js
+++ BMI Calculator/bmi.js
@@ -1,1 +1,36 @@
-console.log('bmi calculator');
+
+const form = document.querySelector('form')
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+    const results = document.querySelector("#results")
+    console.log(height, weight);
+
+
+    if (height === '' || height < 0 || isNaN(height)) {
+        results.innerHTML = "Please give a valid height"
+
+    } else if (weight === '' || weight < 0 || isNaN(weight)) {
+        results.innerHTML = 'please give valid weight'
+
+    } else {
+        const bmi = (weight / ((height * height) / 1000)).toFixed(2)
+        results.innerHTML = `<span>Your bmi is : ${bmi}</span>`
+
+        if (bmi < 18.6) {
+            results.innerHTML = `<span>YOUR ${bmi} BMI IS UNDER WEIGHT</span>`
+        }
+        else if (bmi > 18.6 && bmi <= 24.9) {
+            results.innerHTML = `<span>YOUR ${bmi} BMI IS NORMAL RANGE</span>`
+        }
+        else if (bmi > 24.9) {
+            results.innerHTML = `<span> ${bmi} OVERWEIGHT</span>`
+        }
+
+    }
+
+
+})
+
